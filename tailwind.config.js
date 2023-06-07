51361973bf1bd3f5cb263e5c27ae478fb5f35eba
Pedro Hourrutiner <pedrodavidhourrutinerdiaz@gmail.com>
2023-06-06 20:00:56 -0400

    Main en vista móvil completado

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,15 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     fontFamily: {
-      poppins: ["Poppins", "sans-serif"]
+      poppins: ["Poppins", "sans-serif"],
+      montserrat: ["Montserrat", "sans-serif"],
+      playfairdisplay: ["PlayfairDisplay", "sans-serif"],
+      roboto: ["Roboto", "sans-serif"]
     },
     extend: {
       colors:{
         'pale-pink':'#D7BDAE',
+        'dark-gray':'#2D2E30',
       },
       screens:{
         'lger': { 'raw': '(min-width: 1200px)'},
